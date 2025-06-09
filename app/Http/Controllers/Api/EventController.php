29fe15d97c1c5231e51587fdf28881721311c003
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-08 21:07:35 -0300

    Erro de grafia

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
-use Illuminate\Http\Request;W
+use Illuminate\Http\Request;
 use App\Models\Event;
 
 class EventController extends Controller
