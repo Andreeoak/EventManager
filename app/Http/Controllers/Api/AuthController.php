0368ed2b1ef577cfaa06897d2043c7a77b1cc679
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-06-12 20:24:28 -0300

    Seeting up Auth [feature]

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate(['email'=> 'required|email', 'password'=> 'required']);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+
+        if(!$user)
+        {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        if(!Hash::check($request->password, $user->password))
+        {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Logout successful'], 200);
+    }
+}
